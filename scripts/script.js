02ae456acb6fc0317581ae9f1aab483edb6d11d4
AndreyLyulekin <andreilyulekin@yandex.ru>
2022-11-04 13:30:28 +1000

    tens try

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,31 @@
-
-//Открыть/закртыть форму редактирования
-
 let editForm = document.querySelector('.profile__edit-btn');
-let overlayOpen = document.querySelector('.overlay');
-
-editForm.addEventListener('click', function () {
-  overlayOpen.classList.toggle('overlay__on');
-});
-
-let overlayCLose = document.querySelector('.overlay__close-button');
-
-overlayCLose.addEventListener('click', function () {
-  overlayOpen.classList.remove('overlay__on');
-});
-
-//Изменить через форму, имя и описание профиля + закрытие поп-ап через сохранить
-
-let formElement = document.querySelector('.overlay__form');
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-let nameInput = document.querySelector('.overlay__input_type_name').value;
-let jobInput = document.querySelector('.overlay__input_type_job').value;
+let popup = document.querySelector('.popup');
+let formExit = document.querySelector('.popup__exit');
 let newName = document.querySelector('.profile__name');
-newName.textContent = nameInput;
 let newJob = document.querySelector('.profile__subtitle');
-newJob.textContent = jobInput;
-overlayOpen.classList.remove('overlay__on');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let popupForm = document.querySelector('.popup__container');
+
+function openPopup() {
+    popup.classList.add ('popup_opened');
+    nameInput.value = newName.textContent;
+    jobInput.value = newJob.textContent;
+    }
+
+function closePopup() {
+    popup.classList.remove ('popup_opened');
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    newName.textContent = nameInput.value;
+    newJob.textContent = jobInput.value;
+    
+    closePopup();
+}
+
+editForm.addEventListener('click', openPopup);
+formExit.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', formSubmitHandler); 
